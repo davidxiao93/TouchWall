8bb7c1d4b95141cd45645399ca968bb0c616d948
davidxiao93 <davidxiao93@gmail.com>
2015-04-27 18:25:20 +0100

    fixed typos and shifted variables closer to their use

--- Gesture.cs
+++ Gesture.cs
@@ -86,23 +86,8 @@ namespace TouchWall
         private void ProcessGesture()
         {
             
-
             float prevX = PrevX[Id];
             float prevY = PrevY[Id];
-
-            // 1 means it never uses the previous value
-            //const float smoothingFactor = 0.25f;
-
-            // Experimental - but seems to give positive results :D
-            // Useing the difference between current vvalues of X and Y with PrevX and PrevY to determine smoothing factor
-            // instead of using a constant
-            // normal exponential smoothing keeps its smoothing factor constant
-            float tempDistance = (float)Math.Sqrt((X - prevX)*(X - prevX) + (Y - prevY)*(Y - prevY));
-            float smoothingFactor = tempDistance*10 + 0.01f;
-            if (smoothingFactor > 1)
-            {
-                smoothingFactor = 1;
-            }
 
             if (prevX < -1000 || prevY < -1000)
             {
@@ -111,6 +96,21 @@ namespace TouchWall
             }
             else
             {
+
+                // 1 means it never uses the previous value
+                //const float smoothingFactor = 0.25f;
+
+                // Experimental - but seems to give positive results :D
+                // Using the difference between current vvalues of X and Y with PrevX and PrevY to determine smoothing factor instead of using a constant
+                // normal exponential smoothing keeps its smoothing factor constant
+                float tempDistance = (float)Math.Sqrt((X - prevX) * (X - prevX) + (Y - prevY) * (Y - prevY));
+                float smoothingFactor = tempDistance * 10 + 0.01f;
+                if (smoothingFactor > 1)
+                {
+                    smoothingFactor = 1;
+                }
+
+
                 X = smoothingFactor * X + (1 - smoothingFactor) * prevX;
                 Y = smoothingFactor * Y + (1 - smoothingFactor) * prevY;
                 PrevX[Id] = X;
